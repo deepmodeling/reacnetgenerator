f9e8d19c9d2149507d266ebabbf64504fbf0bb7a
Jin-Zhe Zeng <njzjz@qq.com>
2018-06-24 02:54:30 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(name='ReacNetGenerator',
-      version='1.1.5',
+      version='1.1.7',
       description='Reaction Network Generator',
       keywords="reaction network",
       url='https://github.com/njzjz/ReacNetGenerator',
