381f167789b804ad446c506b60f5b10eaf62acac
Jinzhe Zeng <njzjz@qq.com>
2019-01-22 15:02:18 +0800

    check md5 when testing

--- reacnetgenerator/test.py
+++ reacnetgenerator/test.py
@@ -3,6 +3,7 @@
 
 import os
 import unittest
+import hashlib
 
 import reacnetgenerator
 import requests
@@ -18,21 +19,37 @@ def download_file(url, local_filename):
     return local_filename
 
 
+def checkmd5(filename):
+    if not os.path.isfile(filename):
+        return
+    myhash = hashlib.md5()
+    with open(filename, 'rb') as f:
+        while True:
+            b = f.read(8096)
+            if not b:
+                break
+        myhash.update(b)
+    return myhash.hexdigest()
+
+
 class TestReacNetGen(unittest.TestCase):
     def test_reacnetgen(self):
         # download bonds.reaxc
         file_url = "https://drive.google.com/uc?authuser=0&id=1CJ22BZTh2Bg3MynHyk_CVZl0rcpSQzRn&export=download"
+        file_md5 = "d41d8cd98f00b204e9800998ecf8427e"
+
         folder = "test"
         filename = "bonds.reaxc"
         bondfilename = os.path.join(folder, filename)
 
-        if not os.path.exists(folder):
-            os.makedirs(folder)
-        print(f"Downloading {filename} ...")
-        download_file(file_url, bondfilename)
+        while not os.path.isfile(bondfilename) or checkmd5(bondfilename) != file_md5:
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+            print(f"Downloading {filename} ...")
+            download_file(file_url, bondfilename)
 
-        r = reacnetgenerator.ReacNetGenerator(inputfilename=bondfilename, atomname=[
-                                              'H', 'O'], inputfiletype='lammpsbondfile', runHMM=True)
+        r=reacnetgenerator.ReacNetGenerator(inputfilename = bondfilename, atomname = [
+                                              'H', 'O'], inputfiletype = 'lammpsbondfile', runHMM = True)
         r.runanddraw()
 
         print("Here are reactions:")
