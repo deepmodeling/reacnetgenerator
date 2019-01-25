875a8a8ab6b9820f6dbe7dc86a440bfa88566c38
stickler-ci <support@stickler-ci.com>
2019-01-25 14:21:22 +0000

    Fixing style errors.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from os import path
 
-if __name__=='__main__':
+if __name__ == '__main__':
     this_directory = path.abspath(path.dirname(__file__))
     with open(path.join(this_directory, 'docs', 'README.md'), encoding='utf-8') as f:
         long_description = f.read()
