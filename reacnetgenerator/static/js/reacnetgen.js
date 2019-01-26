9d3e449582ea07a5c27117341d76e518b6a74eb4
Jinzhe Zeng <njzjz@qq.com>
2019-01-26 15:36:43 +0800

    fix js issues

--- reacnetgenerator/static/js/reacnetgen.js
+++ reacnetgenerator/static/js/reacnetgen.js
@@ -1,42 +1,44 @@
-var canvas = document.getElementById("canvas");
+/* global jsnx */
+/* global linkreac */
+var canvas = $document.getElementById("canvas");
 
 var G = new jsnx.Graph();
-var timer=null;
+var timer = null;
 
 jsnx.draw(G, {
-    element: canvas,
-    layoutAttr: {
-        charge: -1000,
-        linkDistance: 300,
-        gravity: 0.05,
+    "element": canvas,
+    "layoutAttr": {
+        "charge": -1000,
+        "linkDistance": 300,
+        "gravity": 0.05,
     },
-    panZoom: {
-        enabled: false
+    "panZoom": {
+        "enabled": false
     },
-    nodeShape: "use",
-    nodeAttr: {
+    "nodeShape": "use",
+    "nodeAttr": {
         title(d) { return d.label; },
-        "xlink:href"(d) {return "#"+d.node+"_border"; }, 
+        "xlink:href"(d) { return "#" + d.node + "_border"; },
         onclick(d) {
-            return "clearTimeout(timer);timer = setTimeout(function(){addnode('"+d.node+"')}, 300);";
-        }, 
-        ondblclick(d) {
-            return "clearTimeout(timer);G.removeNode('"+d.node+"');";
+            return "clearTimeout(timer);timer = setTimeout(function(){addnode('" + d.node + "')}, 300);";
         },
-        width: 100,
-        height: 100,
-        x: -50,
-        y: -50,
-    },
-    nodeStyle: {
-        border: "1px solid #ddd"
-    },
-    edgeStyle: {
-        fill: "#999"
-    },
-    edgeAttr: {
         ondblclick(d) {
-            return "G.removeEdge('"+d.edge[0]+"','"+d.edge[1]+"');";
+            return "clearTimeout(timer);G.removeNode('" + d.node + "');";
+        },
+        "width": 100,
+        "height": 100,
+        "x": -50,
+        "y": -50,
+    },
+    "nodeStyle": {
+        "border": "1px solid #ddd"
+    },
+    "edgeStyle": {
+        "fill": "#999"
+    },
+    "edgeAttr": {
+        ondblclick(d) {
+            return "G.removeEdge('" + d.edge[0] + "','" + d.edge[1] + "');";
         },
     }
 }, true);
@@ -47,12 +49,11 @@ $(".popup-modal").magnificPopup({
     preloader: false,
 });
 
-function addnode(spec){
+function addnode(spec) {
     G.addNode(spec);
-    for (var i in linkreac[spec]){
-        if ({}.hasOwnProperty.call(linkreac[spec], i)) {
-            G.addNode(linkreac[spec][i]);
-            G.addEdge(linkreac[spec][i],spec);
-        }
+    var rightspecs = linkreac[spec]
+    for (var i = 0; i < rightspecs.length; i++) {
+        G.addNode(rightspecs[i]);
+        G.addEdge(rightspecs[i], spec);
     }
 }
