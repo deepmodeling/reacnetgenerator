ed7ce81894deac275224cec8d03661d1f7935f4e
Jinzhe Zeng <jzzeng@stu.ecnu.edu.cn>
2019-02-14 06:01:47 +0800

    Update reacnetgenerator/_path.py

--- reacnetgenerator/_path.py
+++ reacnetgenerator/_path.py
@@ -109,7 +109,7 @@ class _CollectPaths(metaclass=ABCMeta):
                     allmoleculeroute.append(moleculeroute)
                 semaphore.release()
         pool.close()
-        allmoleculeroute = np.unique(np.concatenate(allmoleculeroute), axis=0) if len(
+        allmoleculeroute = np.unique(np.concatenate(allmoleculeroute), axis=0) if allmoleculeroute else np.zeros((0, 2))
         pool.join()
         return allmoleculeroute
 
