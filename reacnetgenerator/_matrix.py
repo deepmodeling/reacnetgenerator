5dca02371f1612ae6c4a8874682d48ee624aa589
Jinzhe Zeng <jzzeng@stu.ecnu.edu.cn>
2019-02-14 08:08:19 +0800

    Update _matrix.py

--- reacnetgenerator/_matrix.py
+++ reacnetgenerator/_matrix.py
@@ -46,7 +46,7 @@ class _GenerateMatrix:
         names = names[names[:, 0] != names[:, 1]]
         if names.size > 0:
             equations = np.unique(names, return_counts=True, axis=0)
-            return zip(*equations)
+            return zip(*map(tuple,equations))
         return []
 
     def _printtable(self, allroute, maxsize=100):
