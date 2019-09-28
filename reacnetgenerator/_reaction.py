0b6d9edfb0c4844e6bd1d0a7222d9c9083ea3f4c
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2019-09-28 13:11:42 -0400

    fix: rewrite reactionabcd

--- reacnetgenerator/_reaction.py
+++ reacnetgenerator/_reaction.py
@@ -5,9 +5,10 @@
 
 '''
 import numpy as np
-from collections import Counter
+from collections import Counter, defaultdict
 
 from .utils import WriteBuffer, run_mp, SharedRNGData
+from .dps import dps_reaction
 
 
 class ReactionsFinder(SharedRNGData):
@@ -21,10 +22,10 @@ class ReactionsFinder(SharedRNGData):
     def findreactions(self, atomeach, conflict):
         allreactions = []
         # atomeach j, atomeach j+1, conflict j, conflict j+1
-        givenarray = [(atomeach[:, j], atomeach[:, j+1], conflict[:, j],
-                        conflict[:, j+1]) for j in range(self.step-1)]
+        givenarray = np.stack((atomeach[:-1], atomeach[1:],
+                               conflict[:-1], conflict[1:]), axis=1)
         results = run_mp(self.nproc, func=self._getstepreaction, l=givenarray,
-                            total=self.step-1, desc="Analyze reactions (A+B->C+D)", unit="timestep")
+                         total=self.step-1, desc="Analyze reactions (A+B->C+D)", unit="timestep")
         for networks in results:
             allreactions.extend(networks)
         # reaction with SMILES
@@ -35,49 +36,33 @@ class ReactionsFinder(SharedRNGData):
                     f.append(f"{number} {reaction}")
 
     def _getstepreaction(self, item):
-        atomeachj, atomeachjp1, conflictj, conflictjp1 = item
-        networks = []
-        modifiedatoms = np.where(np.not_equal(atomeachj, atomeachjp1))[0]
-        for i in modifiedatoms:
-            networks.append([({atomeachj[i], self.CONFLICT} if conflictj[i] else {atomeachj[i]}), ({
-                            atomeachjp1[i], self.CONFLICT} if conflictjp1[i] else {atomeachjp1[i]})])
-        a1 = 0
-        while a1 < len(networks):
-            n1 = networks[a1]
-            for a2, n2 in enumerate(networks[a1+1:], a1+1):
-                if n1[0] & n2[0] or n1[1] & n2[1]:
-                    networks.pop(a2)
-                    networks.pop(a1)
-                    networks.append([n1[0] | n2[0], n1[1] | n2[1]])
-                    break
-            else:
-                a1 += 1
-
+        # atomeachj, atomeachjp1, conflictj, conflictjp1
+        modifiedatoms = np.not_equal(item[0], item[1])
+        # covert to dict
+        reactdict = [defaultdict(list), defaultdict(list)]
+        for mol in item[:, modifiedatoms].T:
+            reactdict[0][mol[0]].append(mol[1])
+            reactdict[1][mol[1]].append(mol[0])
+            if mol[2]:
+                reactdict[0][mol[0]].append(self.CONFLICT)
+            if mol[3]:
+                reactdict[1][mol[1]].append(self.CONFLICT)
+        networks = dps_reaction(reactdict)
         # remove empty AND conflict
-        a = 0
-        while a < len(networks):
-            n = networks[a]
-            if self.EMPTY in n[0] or self.EMPTY in n[1] or self.CONFLICT in n[0] or self.CONFLICT in n[1]:
-                networks.pop(a)
-            else:
-                a += 1
+        new_networks = []
+        for nn in networks:
+            if not (self.EMPTY in nn[0] or self.EMPTY in nn[1] or self.CONFLICT in nn[0] or self.CONFLICT in nn[1]):
+                new_networks.append(nn)
         # reaction with SMILES name like A+B->C+D
-        networks = [self._filterspec(reaction) for reaction in networks]
-        return networks
+        reactions = [self._filterspec(reaction) for reaction in new_networks]
+        return reactions
 
     def _filterspec(self, reaction):
-        leftname, rightname = [[self.mname[spec-1]
-                                for spec in side] for side in reaction]
+        leftname, rightname = (
+            Counter(self.mname[np.array(side)-1]) for side in reaction)
         # remove duplicate species
-        i = 0
-        while i < len(leftname):
-            for j, _ in enumerate(rightname):
-                if leftname[i] == rightname[j]:
-                    leftname.pop(i)
-                    rightname.pop(j)
-                    break
-            else:
-                i += 1
+        leftname -= rightname
+        rightname -= leftname
         if leftname and rightname:
-            return '->'.join(('+'.join(sorted(side)) for side in (leftname, rightname)))
+            return '->'.join(('+'.join(sorted(side.elements())) for side in (leftname, rightname)))
         return None
