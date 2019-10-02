35f2c3239d89c8ab6d6ab68e5a72ec72c15980bd
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2019-10-02 13:53:30 -0400

    chore: fix small typo (#460)
    * chore: fix small typo
    
    * Update .gitignore

--- reacnetgenerator/_detect.py
+++ reacnetgenerator/_detect.py
@@ -101,8 +101,8 @@ class _Detect(SharedRNGData, metaclass=ABCMeta):
         pass
 
     def _connectmolecule(self, bond, level):
-        return list([b' '.join((listtobytes(mol),
-                                listtobytes(bondlist))) for mol, bondlist in zip(*dps(bond, level))])
+        return list([b''.join((listtobytes(mol),
+                               listtobytes(bondlist))) for mol, bondlist in zip(*dps(bond, level))])
 
     def _writemoleculetempfile(self, d):
         with WriteBuffer(tempfile.NamedTemporaryFile('wb', delete=False)) as f:
