ac2a06aba1c3ddba566458b9a28759dc4f246a56
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-04-01 22:59:19 -0400

    fix: add "oldest-supported-numpy" and remove numpy version range (#1084)
    * fix: add "oldest-supported-numpy" and remove numpy version range
    
    see https://github.com/scipy/oldest-supported-numpy for details
    
    * remove numpy setup_requires

--- setup.py
+++ setup.py
@@ -120,7 +120,7 @@ if __name__ == '__main__':
           packages=find_packages(),
           python_requires='~=3.6',
           install_requires=[
-              'numpy>=1.15', 'scipy>=0.20.1', 'networkx',
+              'numpy', 'scipy>=0.20.1', 'networkx',
               'matplotlib', 'hmmlearn>=0.2.1',
               'ase', 'scour', 'tqdm',
               'coloredlogs',
@@ -144,7 +144,7 @@ if __name__ == '__main__':
               'setuptools_scm',
               'pytest-runner',
               'cython>=0.16',
-              'numpy>=1.15',
+              'numpy',
           ],
           package_data={
               'reacnetgenerator': ['static/webpack/bundle.html',
