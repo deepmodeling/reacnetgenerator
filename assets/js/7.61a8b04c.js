88683906775a627ba32f38be388289a3c400eade
njzjz <jinzhe.zeng@rutgers.edu>
2021-05-02 08:18:11 +0000

    Deploying to gh-pages from @ tongzhugroup/reacnetgenerator@4d2e0b96856dbc81b00706eb32fa537ed0baf40e 🚀

---
+++ assets/js/7.61a8b04c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{367:function(e,t,n){"use strict";n.r(t);n(46),n(67);var o={name:"Redirect",props:{to:{type:String,required:!0}},beforeMount:function(){document.location.replace(this.to)}},r=n(45),i=Object(r.a)(o,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);t.default=i.exports}}]);
