e2f00dd647e79b07bed03af31b70a6cb69d5af1a
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-05-05 03:27:01 -0400

    chore: polish reacnetgen.js (#1143)
    * chore: polish reacnetgen.js
    
    * bugfix
    
    * bugfix
    
    * bugfix
    
    * fix typo
    
    * bugfix
    
    * fix
    
    * bugfix
    
    * remove async
    
    * should be filter
    
    * bugfix

---
+++ reacnetgenerator/static/webpack/select.js
@@ -0,0 +1,23 @@
+/**
+ * Select from a list.
+ * @param {array} select The list to select.
+ * @param {array} from The list to select from.
+ * @param {function} func The function to check if an element should be selected, which
+ *      has two parameters: element, select.
+ * @return {array} The selected list.
+ */
+const search = (select, from, func) => {
+    // if there is no select, return all of list
+    if (!select.length) return from;
+    return from.filter(element => func(element, select));
+}
+
+const searchspecies = (select, from) => search(select, from, (element, select) => {
+    return select.includes(element["s"]);
+});
+
+const searchreaction = (select, from) => search(select, from, (element, select) => {
+    return element["l"].concat(element["r"]).some(spec => select.includes(spec));
+});
+
+module.exports = { searchspecies, searchreaction };
