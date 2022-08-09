36aa2cb074258070cedffdd26f20068c4236ff93
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-08-09 19:45:21 -0400

    write scm version to the package (#1654)

--- reacnetgenerator/_version.py
+++ reacnetgenerator/_version.py
@@ -5,4 +5,4 @@ from pkg_resources import DistributionNotFound, get_distribution
 try:
     __version__ = get_distribution('reacnetgenerator').version
 except DistributionNotFound:
-    __version__ = ''
+    __version__ = 'Unknown'
