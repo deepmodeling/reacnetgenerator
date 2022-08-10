70df2e623f4f8d9053423cd8281cdcb9326c206c
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-08-09 22:42:12 -0400

    fix release version

--- reacnetgenerator/_version.py
+++ reacnetgenerator/_version.py
@@ -3,6 +3,9 @@
 """Obtain the version."""
 from pkg_resources import DistributionNotFound, get_distribution
 try:
-    __version__ = get_distribution('reacnetgenerator').version
-except DistributionNotFound:
-    __version__ = 'Unknown'
+    from ._version2 import version as __version__
+except ImportError:
+    try:
+        __version__ = get_distribution('reacnetgenerator').version
+    except DistributionNotFound:
+        __version__ = 'Unknown'
