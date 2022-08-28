9af875acc16cf80c759910c76d01f6f5178d0c46
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-08-28 18:31:44 -0400

    web: load popper from script

--- reacnetgenerator/static/webpack/reacnetgen.js
+++ reacnetgenerator/static/webpack/reacnetgen.js
@@ -16,6 +16,7 @@ import './reacnetgen.scss'
 global.$ = global.jQuery = require('jquery');
 global.regeneratorRuntime = require("regenerator-runtime");
 require('bootstrap');
+require('@popper.js/core');
 global.anime = window.anime = require('animejs');
 require('jsrender');
 require('paginationjs');
