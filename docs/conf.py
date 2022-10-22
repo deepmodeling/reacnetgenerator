b56eb5619e971961a12d4200faca022fcc18c404
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-10-22 09:01:40 -0400

    docs: document JS API

--- docs/conf.py
+++ docs/conf.py
@@ -41,6 +41,7 @@ extensions = [
     'numpydoc',
     'sphinx-favicon',
     'deepmodeling_sphinx',
+    'sphinx-js',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -107,6 +108,8 @@ mathjax3_config = {
     "loader": {"load": ['[tex]/mhchem']},
     "tex": {"packages": {'[+]': ['mhchem']}},
 }
+
+js_source_path = "../reacnetgenerator/static/webpack"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
