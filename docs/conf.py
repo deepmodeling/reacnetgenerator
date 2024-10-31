080511a42e2884fd3674b58cc01ec94493983fc3
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-10-31 16:13:17 -0400

    docs: switch to sphinx_book_theme

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "pydata_sphinx_theme"
+html_theme = "sphinx_book_theme"
 html_logo = "_static/reacnetgen.svg"
 html_static_path = ["_static"]
 html_js_files = ["https://unpkg.com/bilitube@0/dist/bilitube.min.js"]
@@ -107,7 +107,7 @@ favicons = [
     },
 ]
 
-enable_deepmodeling = False
+enable_deepmodeling = True
 
 myst_enable_extensions = [
     "dollarmath",
